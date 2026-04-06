63c2e8636120a8d29e607647c32709eeeefe2f8e
Kanjurito <robin.maxime71@gmail.com>
2026-04-06 19:45:40 +0200

    update plugin.zsh

--- plugin.zsh
+++ plugin.zsh
@@ -28,16 +28,15 @@ achievements() {
         echo " ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝"
         echo -e "\e[0m"
 
-    echo -e "\e[1mSuccès débloqués :\e[0m"
+    echo -e "\e[1mUnlocked achievements :\e[0m"
 
-    # On vérifie s'il y a au moins un fichier .lock
     if ls "$dir"/*.lock &>/dev/null; then
         for file in "$dir"/*.lock; do
             name=$(basename "$file" .lock | tr '_' ' ')
             echo -e "  \e[1;32m[✓]\e[0m $name"
         done
     else
-        echo -e "  \e[1;30m(Aucun succès débloqué pour le moment)\e[0m"
+        echo -e "  \e[1;30m(No achievements :/)\e[0m"
     fi
     echo ""
 }
